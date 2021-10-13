4224fc72c1f871088b3fe1cf2e755bff28e48853
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-10-13 20:51:24 -0300

    Primeiro commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div id="app">
+		<Header title="Gerenciamento de Produção"/>
+		<Content />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from "./components/template/Header.vue"
+import Content from "./components/template/Content.vue"
+import Footer from "./components/template/Footer.vue"
+
+export default {
+	name: "App",
+	components: {Header, Content, Footer}
+}
+</script>
+
+<style>
+	*{
+		font-family: "Lato", sans-serif;
+	}
+
+	body{
+		margin: 0;
+
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 70px 1fr 40px;
+		grid-template-areas: 
+		"header"
+		"content"
+		"footer";
+	}
+</style>
---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+      <h1 class="title">
+          {{ title }}
+      </h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style>
+    .header{
+        grid-area: header;
+        background: blue;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title {
+        font-size: 1.2rem;
+        color: #fff;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+        }
+
+    .title a{
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
